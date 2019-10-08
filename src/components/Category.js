b9212e77df04a8c0f7ad140fd5a4578c020080ed
Mollie McIntyre <molliemcintyre@Mollies-Air.attlocal.net>
2019-10-07 23:22:31 -0500

    Added lyrics form and get lyrics by category

--- src/components/Category.js
+++ src/components/Category.js
@@ -8,66 +8,89 @@ class Category extends Component {
       category: this.props.match.params.id,
       lyric: '',
       artist: '',
-      lyrics: {},
+      song: '',
+      lyrics: [],
     }
-    this.catLyrics = db.ref().child('lyrics');
-    this.handleNewLyrics = snap => {
-      console.log(snap.val());
-      if (snap.val()) this.setState({ lyrics: snap.val() });
-    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  componentDidMount() {
-    this.catLyrics.on('value', this.handleNewLyrics);
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
   }
 
-  componentWillUnmount() {
-    this.chatRoom.off('value', this.handleNewLyrics);
-  }
-  handleLyricChange = e => this.setState({ lyric: e.target.value });
-  handleArtistChange = e => this.setState({ artist: e.artist.value });
-  handleClick = e => {
-    // register the lyric
-    db.ref().child('lyrics').push({
+  handleSubmit(e) {
+    e.preventDefault();
+    const lyricsRef = firebase.database().ref('lyrics');
+    const lyric = {
       lyric: this.state.lyric,
       artist: this.state.artist,
+      song: this.state.song,
+      category: this.state.category
+    }
+    lyricsRef.push({
+      lyric: this.state.lyric,
+      artist: this.state.artist,
+      song: this.state.song,
+      category: this.state.category,
     });
-  };
-
-  
+    this.setState({
+      lyric: '',
+      artist: '',
+      song: ''
+    });
+  }
 
   componentDidMount() {
     const lyricsRef = firebase.database().ref('lyrics');
-    lyricsRef.on('value', (snapshot) => {
+    lyricsRef.orderByChild("category").equalTo(this.state.category).on('value', (snapshot) => {
       let lyrics = snapshot.val();
-      console.log(lyrics);
       let newState = [];
+
       for (let lyric in lyrics) {
-        console.log(lyric);
         newState.push({
-          id: lyrics
+          id: lyric,
+          lyric: lyrics[lyric].lyric,
+          artist: lyrics[lyric].artist,
+          song: lyrics[lyric].song,
+          category: lyrics[lyric].category
         });
       }
       this.setState({
-        categories: newState
+        lyrics: newState
       });
     });
   }
 
   render() {
+    const {lyrics} = this.state;
     return(
       <div>
-      <h1>Category: {this.state.category } </h1>
-
-      <form onSubmit={this.handleSubmit}>
-        <input type="text" name="lyric" placeholder="New Lyric" onChange={this.handleChange} value={this.state.lyric} />
-        <input type="text" name="artist" placeholder="Artist" onChange={this.handleChange} value={this.state.artist} />
-        <button>Add Lyric</button>
-      </form>
-
-
+        <div className="form">
+          <h1>Category: {this.state.category } </h1>
+            <form onSubmit={this.handleSubmit}>
+              <input type="text" name="lyric" placeholder="New Lyric" onChange={this.handleChange} value={this.state.lyric} />
+              <input type="text" name="artist" placeholder="Artist" onChange={this.handleChange} value={this.state.artist} />
+              <input type="text" name="song" placeholder="Song" onChange={this.handleChange} value={this.state.song} />
+              <button>Add Lyric</button>
+            </form>
+        </div>
+        <div className="lyricsList">
+          <ul>
+            {this.state.lyrics.map((lyric) => {
+              return (
+                <li key={lyric.id}>
+                  <h3>{lyric.lyric}</h3>
+                  <p>Artist: {lyric.artist}</p>
+                  <p>Song: {lyric.song}</p>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
       </div>
-
     );
   }
 }
